b1a13965ae056199db5b306e8fc790c78de5920a
VargaGyula <gyuszikavarga2006@gmail.com>
2025-09-12 12:36:03 +0200

    ciklusok

--- ciklus.py
+++ ciklus.py
@@ -18,3 +18,32 @@ def paros_szamok_darabszama(lista):
 
 print(f"A lista elemeinek összege:{osszegez(lista)}")
 print(f"A lista páros elemeinek darabszáma: {paros_szamok_darabszama(lista)}")
+
+# Írass ki egy sorba 5 *-ot
+
+print("*"*5)
+
+for i in  range(5):
+    print("*", end="")
+print()
+
+# # rajzolj piramist:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+
+n = 5
+for i in range(1, n+1):
+    print(" "*(n - i)+ "*"*(2*i-1))
+
+ # Szorzótábla 1-től 10-ig
+
+for i in range (1, 11):
+    for j in [1,2,3,4,5,6,7,8,9,10]:
+       print(f"{i*j:3}", end=" ")
+    print()
+    
+   
