1284aa78a721696f54ac423642c3789ed0d73b76
VargaGyula <gyuszikavarga2006@gmail.com>
2025-09-12 10:32:09 +0200

    tesztelés

--- elagazas.py
+++ elagazas.py
@@ -12,3 +12,34 @@ elif gondolt_szam > bekert_szam:
     print("Nagyobbra gondoltam!")
 else:
     print("Kisebbre gondoltam!")
+
+#Készíts függvényt előjel néven, mely átvesz egy egész számot és az előjelét adja vissza
+
+def elojel(szam):
+    if szam >= 0:
+        return "+"
+    else:
+        return "-"
+
+szam = int(input("Kérek egy számot előjellel: "))
+
+print(f"A {szam} elojele: {elojel(szam)}.")
+
+print(f"A {szam} elojele: {'+' if szam>=0 else '-'}.")
+
+def tesztesetek():
+    teszt(5,"+");
+    teszt(-3,"-")
+    teszt(-1,"-")
+    teszt(0,"+")
+    teszt(1,"+")
+    #...
+
+
+def teszt(szam, elvart_elojel):
+    if elojel(szam) == elvart_elojel:
+        print(f"elojel({szam})=={elvart_elojel} true")
+    else:
+        print(f"elojel({szam})=={elvart_elojel} false")
+
+tesztesetek()
